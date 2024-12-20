16d0ed39d5b5e8156e17acd96d1b1aef4a6d725d
ShionNakamura <shionnakamura0120@gmail.com>
2024-12-20 22:11:07 +0900

    Initial Commit

---
+++ SwiftulMapApp/SwiftulMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftulMapAppApp.swift
+//  SwiftulMapApp
+//
+//  Created by 仲村士苑 on 2024/12/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftulMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftulMapAppTests/SwiftulMapAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftulMapAppTests.swift
+//  SwiftulMapAppTests
+//
+//  Created by 仲村士苑 on 2024/12/20.
+//
+
+import Testing
+@testable import SwiftulMapApp
+
+struct SwiftulMapAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
